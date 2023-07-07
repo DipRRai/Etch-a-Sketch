9686311f1848e833f299eb7740f839eae87f047a
superderped <24651666+superderped@users.noreply.github.com>
2023-07-08 00:55:57 +1000

    added 2 point generation, randomise

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -7,20 +7,40 @@ const colorpicker = document.querySelector('.colorpalette');
 const sizeText = document.querySelector('.sizeText')
 const displayWindow = document.querySelector('.lower')
 const randomise = document.querySelector('.randomise')
+const start = document.querySelector('.start')
 const matrix = [];
+var startPoint = [0,0];
+var endPoint = [0,0];
+var matrixSize = size.value;
 
 randomise.addEventListener('click', funcRandomise);
 clear.addEventListener('click', clearcanvas);
 setSize.addEventListener('click', resetGrid);
 size.addEventListener('input', updateSize);
+start.addEventListener('click', funcStart)
 colorpicker.oninput = (e) => paintColor = e.target.value;
 
 function funcRandomise(){
   var i = 1;
-  for (const square of matrix){
-    setTimeout(() => square.style.backgroundColor = "#"+Math.floor(Math.random()*16777215).toString(16), 0.1*i);
-    i++;
+  for (const line of matrix){
+    for (const square of line){
+      setTimeout(() => square.style.backgroundColor = "#"+Math.floor(Math.random()*16777215).toString(16), 0.1*i);
+      i++;
+    }
   }
+}
+
+function funcStart(){
+  matrix[startPoint[0]][startPoint[1]].style.backgroundColor = "black";
+  matrix[endPoint[0]][endPoint[1]].style.backgroundColor = "black";
+  startPoint = []
+  endPoint = []
+  startPoint.push(Math.floor(Math.random()*(matrixSize-1)))
+  startPoint.push(Math.floor(Math.random()*(matrixSize-1)))
+  endPoint.push(Math.floor(Math.random()*(matrixSize-1)))
+  endPoint.push(Math.floor(Math.random()*(matrixSize-1)))
+  matrix[startPoint[0]][startPoint[1]].style.backgroundColor = "red";
+  matrix[endPoint[0]][endPoint[1]].style.backgroundColor = "blue";
 }
 
 function updateSize(){
@@ -50,6 +70,7 @@ function color(e){
 }
 
 function makeGrid(rows, cols) {
+  matrixSize = rows;
   let tempGrid = container
   tempGrid.style.textAlign = 'centre';
   tempGrid.style.display = 'grid';
@@ -57,6 +78,7 @@ function makeGrid(rows, cols) {
   tempGrid.style.gridTemplateRows = 'repeat('+ cols +', auto)';
   var paddingSize = (container.clientHeight/cols)
   for (let i = 0; i<rows; i++){
+    matrix.push([])
     for (let j = 0; j<cols; j++){
       let tempcol = document.createElement('div');
       tempcol.addEventListener('mouseover', color);
@@ -64,17 +86,29 @@ function makeGrid(rows, cols) {
       tempcol.style.backgroundColor = 'black';
       tempcol.style.padding = (paddingSize)+"px";
       tempGrid.appendChild(tempcol);
-      matrix.push(tempcol);
+      matrix[i].push(tempcol);
     }
   }
+  console.log(matrix)
 }
+
+function startend(){
+
+}
+
 
 let mouseDown = false
 let paintColor = 'red';
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
-makeGrid(64,64);
+makeGrid(size.value,size.value);
+
+if (matrix[0][0].backgroundColor == "black"){
+  console.log("black");
+} else {
+  console.log("bruh")
+}
 
 //comment
   
